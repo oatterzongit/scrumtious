dc636dd4e81f1e8055b91bbeaff8b8cbab1d4d09
Oat Asdon <o.asdon@gmail.com>
2016-03-12 14:46:37 -0800

    Update user model schema
    Replace 'name' with first_name and last_name.
    Add trello_id

--- models/user.js
+++ models/user.js
@@ -2,8 +2,9 @@ var mongoose = require('mongoose'),
     debug    = require('debug')('app:models');
 
 var userSchema = new mongoose.Schema({
-  name:   String,
-  handle: String
+  first_name:   { type: String, required: true },
+  last_name:    { type: String, required: true },
+  trello_id:    { type: String, required: true }
 });
 
 var User = mongoose.model('User', userSchema);
