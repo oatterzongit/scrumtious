753a9bd4363f6fc0bf05349f09451e0f8d7a24f8
h4w5 <pj@ga.co>
2016-03-15 10:35:03 -0700

    add user creation to trello auth

--- config/passport.js
+++ config/passport.js
@@ -1,39 +1,69 @@
-var User = require('../models/users');
-var TrelloStrategy = require('passport-trello').Strategy;
+var debug = require("debug")("app:auth");
+
+var User = require("../models/users");
+var TrelloStrategy = require("passport-trello").Strategy;
 
 module.exports = function(passport){
-  passport.serializeUser(function(user, done) {
-    done(null, user._id);
+  passport.serializeUser(function(user, next) {
+    console.log("CURRENT USER IN SESSION!");
+    debug("--> serializing user:\n", user, "\n\n");
+    next(null, user._id);
   });
 
-  passport.deserializeUser(function(id, done) {
+  passport.deserializeUser(function(id, next) {
     User.findById(id, function(err, user) {
-      console.log('deserializing user:',user);
-      done(err, user);
+      console.log("--> deserializing user:\n", user, "\n\n");
+      next(err, user);
     });
   });
 
-  TrelloStrategy = require('passport-trello').Strategy
+  TrelloStrategy = require("passport-trello").Strategy
 
-  passport.use('trello', new TrelloStrategy({
-      consumerKey: process.env.CID,
-      consumerSecret: process.env.CLS,
-      callbackURL: 'http://localhost:3000/auth/trello/callback',
+  passport.use("trello", new TrelloStrategy({
+      consumerKey:       process.env.CID,
+      consumerSecret:    process.env.CLS,
+      callbackURL:       "http://localhost:3000/auth/trello/callback",
       passReqToCallback: true,
+
       trelloParams: {
-        scope: "read, write, account",
-        name: "Scrumtious",
+        scope:      "read, write, account",
+        name:       "Scrumtious",
         expiration: "never"
       }
-    }, function (req, token, tokenSecret, profile, done) {
-        if (!profile.id) {
-          console.log("PROFILE: ", profile, "\n\n\n\n\n\n\n\n")
-        } else {
-          // Check to see if this user already exists
-          // if not,
-          // Here is where you save the user into the database, then call done()
-          console.log("Success")
-          done(null, profile)
-        }
-    }));
+    }, function (req, token, tokenSecret, profile, next) {
+        console.log("\n\nSuccessful login!");
+        debug("  token:  ", token);
+        debug("  secret: ", tokenSecret);
+        debug("  profile:", profile);
+
+        // Check the database for a user with the logged in trello id.
+        User
+        .findOne({trelloId: profile.id}).exec()
+        .then(
+          function(user) {
+            debug("\n\n******SUCCESSFUL SEARCH*********");
+            debug("  User: ", user);
+
+            // When there is no user in the database with this trello id,
+            // create a new user in the database and move on.
+            if (!user) {
+              User.create({
+                trelloId: profile.id,
+                userName: profile._json.username,
+                fullName: profile._json.fullName
+              })
+              .then(
+                function(user) { next(null, user); },
+                function(err)  { next(err); }
+              )
+
+            // Else, when the user is in the database, move on.
+            } else {
+              next(null, user);
+            }
+          },
+          function(err) { next(err); }
+        )
+    })
+  );
 }
